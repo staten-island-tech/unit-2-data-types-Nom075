333bd55c3a0984729ea6552edcbe6b04b81192d7
Nom075 <graytonl@nycstudents.net>
2025-02-04 10:15:30 -0500

    Factors

--- Data.py
+++ Data.py
@@ -1,3 +1,44 @@
-x = 3.141592653589793238462643383279502884197169
+""" x = 3
 y = float(x)
 print(x,y)
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6]) """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+
+
+""" def add(x,y,z):
+    print(x+y+z)
+
+wallet = input("How much money do you have?")
+if wallet == "0":
+    print("You are broke")
+
+day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")  
+
+add("How do you only have", wallet, "$?") """
+
+test = NA
+
+
+def factor(x):
+    for i in x: 
+        test = x % i
+        if test == "0":
+             value.insert()
+
