9efdbe94ffcc0dcca9ab03566b1fb0def0095646
Nom075 <graytonl@nycstudents.net>
2025-02-07 10:07:23 -0500

    Final_Unit2

---
+++ Mad_libs.py
@@ -0,0 +1,17 @@
+Name = input("Enter a person's name")
+Pronoun = input("Enter the person's pronoun he/she")
+Action = input("Enter a action")
+Place = input("Enter a place")
+Name2 = input("Enter another person's name")
+Pronoun2 = input("Enter the person's pronoun he/she")
+Item = input("Enter an item")
+
+
+
+
+
+
+
+
+print(f"As {Name} was {Action} down the street towards {Place}, {Pronoun} bumped into {Name2}! Oh hey {Name2}! I love your {Item}!")
+    
--- number.py
+++ number.py
@@ -4,15 +4,14 @@ def rnumber():
     return random.randint(1,100)
 
 select_number = rnumber()
-print(select_number)
 Tries = 0
 
 
 def numbertrue(Guess):
-    
+    Tries = 0
     while not Guess == select_number:
         if Guess >= select_number:
-            print(f"{Guess}is larger than the number!")
+            print(f"{Guess} is larger than the number!")
             Tries = Tries + 1
             Guess = int(input("Guess again!"))
         elif Guess <= select_number:
@@ -20,10 +19,11 @@ def numbertrue(Guess):
             Tries = Tries + 1
             Guess = int(input("Guess again!"))
     if Guess == select_number:
-           return Guess
+           Tries = Tries + 1
+           return Guess, Tries
         
 
 Guess = input("guess a number!")
-Final_Guess = numbertrue(int(Guess))
+Final_Guess, Final_Tries = numbertrue(int(Guess, Tries))
 
-print(f"{Final_Guess} is the right number! You used {Tries} tries!")
+print(f"{Final_Guess} is the right number! You used {Final_Tries} tries!")
